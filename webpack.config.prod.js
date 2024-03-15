2cd50f0a8b489cb49708177d861e0763c1479b19
WJ-Boshoff <willemwors15@gmail.com>
2024-03-15 21:19:27 +0200

    Refactor

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './RSVP.html',
     }),
     new CopyPlugin({
       patterns: [
